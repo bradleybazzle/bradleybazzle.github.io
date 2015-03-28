9d600d9e8c91f47580eafcdc7762e3fd21e784d0
Tim <uniquelysimilar@yahoo.com>
2015-03-27 19:51:41 -0600

    set active class on current menu selection

---
+++ app/components/navbar/navbarController.js
@@ -0,0 +1,18 @@
+bBazApp.controller('navbarController', ['$scope', function($scope) {
+
+	$scope.setSelection = function(option) {
+		//console.log(option + ' menu option clicked.');
+		$scope.selected = option;
+	};
+
+	$scope.isSelected = function(option) {
+		return $scope.selected === option;
+	};
+
+	$scope.init = function() {
+		$scope.setSelection('about');
+	};
+
+	$scope.init();
+
+}]);
