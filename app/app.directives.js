69bcd25984f6bbfc8e95ca69e90f4b06ff7ed64a
Tim <uniquelysimilar@yahoo.com>
2016-04-01 11:33:38 -0600

    ongoing custom directive impl

--- app/app.directives.js
+++ app/app.directives.js
@@ -3,12 +3,25 @@ bBazApp
   return {
     restrict: 'E',
     scope: {
+      pid: '@',
+      paraClasses: '=',
       title: '@',
       publication: '@',
       issue: '@',
       forthcoming: '@',
       online: '@'
     },
-    templateUrl: 'app/components/stories/baz-story.html'
+    templateUrl: 'app/components/stories/baz-story.html',
+    link: function(scope, elem, attrs) {
+      var pEl = elem.find('p');
+      pEl.attr('id' , scope.pid);
+      console.log("scope.paraClasses:");
+      console.log(scope.paraClasses);
+      /*
+      if (scope.pclass != undefined && scope.pclass.length > 0) {
+        pEl.addClass(scope.pclass);
+      }
+      */
+    }
   };
 });
--- app/components/stories/storiesController.js
+++ app/components/stories/storiesController.js
@@ -1,14 +1,20 @@
 bBazApp.controller('storiesController', ['$scope', function($scope) {
   //console.log("Hello from 'storiesController'");
-  $scope.magellanClass = "";
+  //$scope.magellanClass = "";
+  $scope.paraClasses = {
+    magellanItem: "true",
+    animateBgColor: ""
+  };
 
   $scope.onMouseEnterMagellanImg = function() {
     //console.log("Entered Magellan image");
-    $scope.magellanClass = "magellan-bg-color";
+    //$scope.magellanClass = "magellan-bg-color";
+    $scope.paraClasses.animateBgColor = "true";
   }
 
   $scope.onMouseLeaveMagellanImg = function() {
     //console.log("Left Magellan image");
-    $scope.magellanClass = "";
+    //$scope.magellanClass = "";
+    $scope.paraClasses.animateBgColor = "";
   }
 }]);
