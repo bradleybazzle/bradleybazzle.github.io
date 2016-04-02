e0b995cec6cd9e0201af9c6513ed8e6555692789
Tim <uniquelysimilar@yahoo.com>
2016-04-01 21:13:57 -0600

    created version of story custom directive to take one object attribute instead of multiple attributes

--- app/app.directives.js
+++ app/app.directives.js
@@ -16,13 +16,19 @@ bBazApp
     link: function(scope, elem, attrs) {
       var pEl = elem.find('p');
       pEl.attr('id' , scope.pid);
-      //console.log("scope.paraClasses:");
-      //console.log(scope.paraClasses);
-      /*
-      if (scope.pclass != undefined && scope.pclass.length > 0) {
-        pEl.addClass(scope.pclass);
-      }
-      */
+    }
+  };
+})
+.directive('bazStoryObj', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      story: '=',
+    },
+    templateUrl: 'app/components/stories/baz-story-obj.html',
+    link: function(scope, elem, attrs) {
+      var pEl = elem.find('p');
+      pEl.attr('id' , scope.story.pid);
     }
   };
 });
--- app/components/stories/storiesController.js
+++ app/components/stories/storiesController.js
@@ -7,14 +7,61 @@ bBazApp.controller('storiesController', ['$scope', function($scope) {
 
   $scope.onMouseEnterMagellanImg = function() {
     //console.log("Entered Magellan image");
-    //$scope.magellanClass = "magellan-bg-color";
-    $scope.paraClasses.animateBgColor = "true";
+    //$scope.paraClasses.animateBgColor = "true";
+    $scope.magellan.paraClasses.animateBgColor = "true";
+    $scope.stories[1].paraClasses.animateBgColor = "true";
   }
 
   $scope.onMouseLeaveMagellanImg = function() {
     //console.log("Left Magellan image");
-    //$scope.magellanClass = "";
-    $scope.paraClasses.animateBgColor = "";
+    //$scope.paraClasses.animateBgColor = "";
+    $scope.magellan.paraClasses.animateBgColor = "";
+    $scope.stories[1].paraClasses.animateBgColor = "";
   }
+
+  $scope.stories = [
+    {
+      title: "Test Title from Object Array",
+      publication: "Test Publication",
+      issue: "Test Issue",
+      paraClasses: {
+        testClass1: "true",
+        testClass2: "true"
+      }
+    },
+    {
+      title: "Magellan from Object Array",
+      publication: "The Iowa Review",
+      issue: "41, No. 3 (winter 2011-2012): 1-15",
+      pid: "magellan-item",
+      url: "http://www.iowareview.org/from-the-issue/volume-41-issue-3-%E2%80%94-winter-2011/magellan",
+      paraClasses: {
+        magellanItem: "true",
+        animateBgColor: ""
+      }
+    }
+  ];
+
+  $scope.testStory = {
+    title: "Test Title",
+    publication: "Test Publication",
+    issue: "Test Issue",
+    paraClasses: {
+      testClass1: "true",
+      testClass2: "true"
+    }
+  };
+
+  $scope.magellan = {
+    title: "Magellan",
+    publication: "The Iowa Review",
+    issue: "41, No. 3 (winter 2011-2012): 1-15",
+    pid: "magellan-item",
+    url: "http://www.iowareview.org/from-the-issue/volume-41-issue-3-%E2%80%94-winter-2011/magellan",
+    paraClasses: {
+      magellanItem: "true",
+      animateBgColor: ""
+    }
+  };
 
 }]);
