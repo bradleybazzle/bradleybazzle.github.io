fd22d31c09eb3133a6b4ba12566d0035bce6a50e
Tim <uniquelysimilar@yahoo.com>
2016-04-15 20:03:44 -0600

    updated sql file for other entity

--- assets/db/bradley-other.sql
+++ assets/db/bradley-other.sql
@@ -32,37 +32,47 @@ VALUES
 	"http://www.nereview.com/tag/the-beast-of-yucca-flats/"
 );
 
-/*
-    [
-      {
-        "title": "Flag on the Moon: the Beast of Yucca Flats",
-        "publication": "NER Digital",
-        "issue": "(June 21, 2012):",
-        "type": "creative nonfiction",
-        "pid": "beast-item",
-        "url": "http://www.nereview.com/tag/the-beast-of-yucca-flats/",
-        "pClasses": {
-          "beastItem": "true",
-          "animateBgColor": ""
-        }
-      },
-      {
-        "title": "Maceo Montoya, The Scoundrel and the Optimist",
-        "publication": "Indiana Review",
-        "issue": "32, No. 1 (Summer 2010): 176-177",
-        "type": "book review"
-      },
-      {
-        "title": "The Paris Review Perspective",
-        "publication": "Critical Insights: Benjamin Franklin.",
-        "issue": "Pasadena, CA: Salem Press, 2009",
-        "type": "critical anthology"
-      },
-      {
-        "title": "Bernard Share, Inish",
-        "publication": "Indiana Review",
-        "issue": "31, No. 2 (Winter 2009): 180-181",
-        "type": "book review"
-      }
-    ]
-*/
+INSERT INTO `restapi`.`other`
+(
+`title`,
+`publication`,
+`issue`,
+`type`
+)
+VALUES
+(
+	"Maceo Montoya, The Scoundrel and the Optimist",
+	"Indiana Review",
+	"32, No. 1 (Summer 2010): 176-177",
+	"book review"
+);
+
+INSERT INTO `restapi`.`other`
+(
+`title`,
+`publication`,
+`issue`,
+`type`
+)
+VALUES
+(
+	"The Paris Review Perspective",
+	"Critical Insights: Benjamin Franklin.",
+	"Pasadena, CA: Salem Press, 2009",
+	"critical anthology"
+);
+
+INSERT INTO `restapi`.`other`
+(
+`title`,
+`publication`,
+`issue`,
+`type`
+)
+VALUES
+(
+	"Bernard Share, Inish",
+	"Indiana Review",
+	"31, No. 2 (Winter 2009): 180-181",
+	"book review"
+);
